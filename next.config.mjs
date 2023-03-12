3b00443138fd0247e2301ec641e72c5f3df15bb7
Nidday Mark <nidday.mc.troy@gmail.com>
2023-03-12 06:28:27 +0300

    Updated next config env vars

--- next.config.mjs
+++ next.config.mjs
@@ -31,6 +31,14 @@ const nextConfig =
         images: {
           domains: ["s3.us-east-2.amazonaws.com"],
         },
+        env: {
+          RELAY_API_ENDPOINT: "https://api.brullah.com",
+          BRULLAH_URL: "https://www.brullah.com",
+          CHECKERS_URL: "https://checkers.brullah.com",
+          PAYPAL_CLIENT_ID:
+            "AXIsuLVGpnxP1eAn6k5g8zJYP_blLEFOIwacjvbEOvwJ-z_3M-b5TvFqnScjbnunOcR85yXtJWDVkqTM",
+          YOUTUBE: "https://www.youtube.com/channel/UCfMxzWvxGN45lWiFQODymLA",
+        },
       };
 
 export default nextConfig;
